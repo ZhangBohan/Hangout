020082015a7fcd2b9c10d27b00244e609ef1300a
bohanzhang <bohanzhang@foxmail.com>
2016-06-18 17:11:51 +0800

    use postgresql as default database

--- GuppiesWechat/GuppiesWechat/settings.py
+++ GuppiesWechat/GuppiesWechat/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'GuppiesWechat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'guppies_wechat',
+        'USER': 'bohanzhang',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
