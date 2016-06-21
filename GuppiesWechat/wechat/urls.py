07b8740a3aeb6a16cbef0f6c7705bd512ae4f349
bohanzhang <bohanzhang@foxmail.com>
2016-06-21 22:43:32 +0800

    增加微信授权登录

--- GuppiesWechat/wechat/urls.py
+++ GuppiesWechat/wechat/urls.py
@@ -5,4 +5,5 @@ from wechat import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^callback$', views.callback, name='callback'),
+    url(r'^auth$', views.auth, name='auth'),
 )
--- GuppiesWechat/wechat/views.py
+++ GuppiesWechat/wechat/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render, HttpResponse
+import json
+from urllib.parse import quote_plus
+
+import requests
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, HttpResponse, redirect
 from wechat_sdk import WechatConf, WechatBasic
+import logging
 
 conf = WechatConf(
     token='guppiestoken',
@@ -30,3 +36,46 @@ def callback(request):
         return HttpResponse(echostr)
     else:
         return HttpResponse("error", status=500)
+
+
+def auth(request):
+    code = request.GET.get('code')
+    if not code:
+        encoded_url = quote_plus(request.build_absolute_uri(reverse('auth')))
+        url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&' \
+              'scope=%s&state=%s#wechat_redirect' % (
+                  conf.appid, encoded_url, 'snsapi_userinfo', None)
+        return redirect(url)
+
+    params = {
+        "appid": conf.appid,
+        "secret": conf.appsecret,
+        "code": code,
+        "grant_type": 'authorization_code'
+    }
+
+    response = requests.get('https://api.weixin.qq.com/sns/oauth2/access_token', params=params)
+    logging.info('response:', response.content)
+    print('response:', response.content)
+    if response.status_code != 200:
+        logging.error('wechat auth error. status code: %s' % response.status_code)
+        return HttpResponse(response.content, status=500)
+
+    data = response.json()
+
+    if data.get('errcode'):  # invalid code
+        return HttpResponse(response.content, status=500)
+
+    access_token = data.get('access_token')
+    refresh_token = data.get('refresh_token')
+    openid = data.get('openid')
+    union_id = data.get('unionid')
+
+    rv = requests.get('https://api.weixin.qq.com/sns/userinfo?access_token=%s&openid=%s&lang=zh_CN' % (
+        access_token, openid))
+
+    content = rv.content.decode('utf-8')
+    user_data = json.loads(content)
+
+    print('user_data:', user_data)
+    return HttpResponse(rv.content)
