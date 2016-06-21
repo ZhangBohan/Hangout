0e46eb79a45c9294138affa18e814ff86f3f1f68
ZhangBohan <bohanzhang@foxmail.com>
2016-06-21 18:34:42 +0800

    update conf and callback return

--- GuppiesWechat/wechat/views.py
+++ GuppiesWechat/wechat/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, HttpResponse
 from wechat_sdk import WechatConf, WechatBasic
 
 conf = WechatConf(
-    token='98671d2d-fb6b-49e1-920a-7ae2d034011',
+    token='guppiestoken',
     appid='wxbbb6b25e8943502f',
     appsecret='8140ce54ce5c261fcf1a56fa3e3cc9ca',
-    encrypt_mode='safe',  # 可选项：normal/compatible/safe，分别对应于 明文/兼容/安全 模式
+    encrypt_mode='normal',  # 可选项：normal/compatible/safe，分别对应于 明文/兼容/安全 模式
     # encoding_aes_key='your_encoding_aes_key'  # 如果传入此值则必须保证同时传入 token, appid
 )
 
@@ -17,10 +17,16 @@ def index(request):
 
 
 def callback(request):
-    print(request.GET)
+    signature = request.GET.get('signature')
+    timestamp = request.GET.get('timestamp')
+    nonce = request.GET.get('nonce')
+    echostr = request.GET.get('echostr')
+
+    print(request.GET, conf.appid, signature, timestamp, nonce, echostr)
+
     if wechat_basic.check_signature(request.GET.get('signature'),
                                     request.GET.get('timestamp'),
                                     request.GET.get('nonce')):
-        return HttpResponse("ok")
+        return HttpResponse(echostr)
     else:
         return HttpResponse("error", status=500)
