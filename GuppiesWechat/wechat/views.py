7378f9636fa297f10b67661646f80c1d111727f5
bohanzhang <bohanzhang@foxmail.com>
2016-06-22 22:29:58 +0800

    bug fix

--- GuppiesWechat/wechat/views.py
+++ GuppiesWechat/wechat/views.py
@@ -72,6 +72,7 @@ def auth(request):
 
     access_token = data.get('access_token')
     refresh_token = data.get('refresh_token')
+    expires_in = data.get('expires_in')
     openid = data.get('openid')
     unionid = data.get('unionid')
 
@@ -86,11 +87,13 @@ def auth(request):
         auth = WechatAuth.objects.get(unionid=unionid)
         if auth:
             auth, created = WechatAuth.objects.update_or_create(unionid=unionid,
+                                                                expires_in=expires_in,
                                                                 defaults=dict(access_token=access_token,
                                                                               refresh_token=refresh_token,
                                                                               **user_data))
     if not created:
         auth, created = WechatAuth.objects.update_or_create(openid=openid,
+                                                            expires_in=expires_in,
                                                             defaults=dict(access_token=access_token,
                                                                           refresh_token=refresh_token,
                                                                           **user_data))
