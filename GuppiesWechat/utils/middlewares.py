d2b8ac8cf9dbc0d1eaa0a7b7c0966dc9b97e6137
bohanzhang <bohanzhang@foxmail.com>
2016-07-30 13:13:31 +0800

    增加 cors 中间键  增加错误处理蹭键  优化代码

---
+++ GuppiesWechat/utils/middlewares.py
@@ -0,0 +1,34 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import JsonResponse, HttpResponse, HttpRequest
+
+
+class ProcessCORSMiddleware(object):
+
+    def add_cors_header(self, origin, response):
+        response['Access-Control-Allow-Origin'] = origin
+        response['Access-Control-Allow-Credentials'] = 'true'
+        response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+        response[
+            'Access-Control-Allow-Headers'] = 'reqid, nid, host, x-real-ip, x-forwarded-ip, event-type, event-id, accept, content-type'
+
+    def process_request(self, request):
+        request._dont_enforce_csrf_checks = True  # For csrf
+        if request.method == 'OPTIONS':
+            response = HttpResponse(status=204)
+            self.add_cors_header(request.META.get('HTTP_ORIGIN'), response)
+            return response
+
+    def process_response(self, request, response):
+
+        if request.META.get('HTTP_ACCEPT') == 'application/json':
+            self.add_cors_header(request.META.get('HTTP_ORIGIN'), response)
+        return response
+
+
+class ExceptionMiddleware(object):
+    def process_exception(self, request, exception):
+
+        if request.META.get('CONTENT_TYPE') == 'application/json':
+            if isinstance(exception, ObjectDoesNotExist):
+                return JsonResponse({}, status=404)
+        return JsonResponse({}, status=500)
