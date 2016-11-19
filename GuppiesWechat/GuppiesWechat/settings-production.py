49f1fe7c6981e7b030c8b6a3db43be3f02f5aa0e
bohanzhang <bohanzhang@foxmail.com>
2016-11-20 00:08:28 +0800

    change debug to false
    #17

--- GuppiesWechat/GuppiesWechat/settings-production.py
+++ GuppiesWechat/GuppiesWechat/settings-production.py
@@ -1,6 +1,8 @@
 import raven
 from .settings import *
 
+DEBUG = False
+
 RAVEN_CONFIG = {
     'dsn': 'https://0295646ba4bd4f71bfed6f401833c67a:bf0f6796a3df41d28680e4a903ae7416@sentry.io/82095',
     # If you are using git, you can also automatically configure the
