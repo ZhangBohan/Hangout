9470bf112ec7d0c71f9aa6aa5413d61c80cb850c
bohanzhang <bohanzhang@foxmail.com>
2016-11-19 23:54:02 +0800

    干掉提醒我的逻辑
    fix #20

--- GuppiesWechat/hangout/forms.py
+++ GuppiesWechat/hangout/forms.py
@@ -16,5 +16,5 @@ class ScheduleForm(forms.Form):
 
     started_date = forms.DateTimeField(help_text='开始时间', input_formats=[INPUT_DATE_TIME_FORMAT])
     ended_date = forms.DateTimeField(help_text='结束时间', input_formats=[INPUT_DATE_TIME_FORMAT])
-    notify_me = forms.TypedChoiceField(help_text='提醒我', choices=NOTIFY_CHOICES, coerce=int)
+    # notify_me = forms.TypedChoiceField(help_text='提醒我', choices=NOTIFY_CHOICES, coerce=int)
     notify_other = forms.TypedChoiceField(help_text='提醒别人', choices=NOTIFY_CHOICES, coerce=int)
