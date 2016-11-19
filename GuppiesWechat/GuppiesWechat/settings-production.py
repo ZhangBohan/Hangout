5957ce9743c642d2b230113ec0a77a09a0900b3d
bohanzhang <bohanzhang@foxmail.com>
2016-11-20 00:34:08 +0800

    bug fix

--- GuppiesWechat/GuppiesWechat/settings-production.py
+++ GuppiesWechat/GuppiesWechat/settings-production.py
@@ -13,7 +13,7 @@ RAVEN_CONFIG = {
 
 WECHAT_CONF = WechatConf(
     token='guppiestoken',
-    appid='wxbc67178dd545c3db',
+    appid='wxbbb6b25e8943502f',
     appsecret='8140ce54ce5c261fcf1a56fa3e3cc9ca',
     encrypt_mode='normal',  # 可选项：normal/compatible/safe，分别对应于 明文/兼容/安全 模式
     # encoding_aes_key='your_encoding_aes_key'  # 如果传入此值则必须保证同时传入 token, appid
