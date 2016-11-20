87c3891175ab71f0e8d0cb596423758cfbb7cc97
bohanzhang <bohanzhang@foxmail.com>
2016-11-20 16:48:01 +0800

    bug fix

--- GuppiesWechat/hangout/forms.py
+++ GuppiesWechat/hangout/forms.py
@@ -9,12 +9,15 @@ class ScheduleForm(forms.Form):
         ('', '不提醒'),
     ]
 
-    INPUT_DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M'
+    INPUT_DATE_TIME_FORMATS = [
+        '%Y-%m-%dT%H:%M',
+        '%Y-%m-%dT%H:%M:%S',
+    ]
 
     title = forms.CharField(help_text='标题', max_length=20)
     content = forms.CharField(help_text='描述', max_length=300)
 
-    started_date = forms.DateTimeField(help_text='开始时间', input_formats=[INPUT_DATE_TIME_FORMAT])
-    ended_date = forms.DateTimeField(help_text='结束时间', input_formats=[INPUT_DATE_TIME_FORMAT])
+    started_date = forms.DateTimeField(help_text='开始时间', input_formats=INPUT_DATE_TIME_FORMATS)
+    ended_date = forms.DateTimeField(help_text='结束时间', input_formats=INPUT_DATE_TIME_FORMATS)
     # notify_me = forms.TypedChoiceField(help_text='提醒我', choices=NOTIFY_CHOICES, coerce=int)
     notify_other = forms.TypedChoiceField(help_text='提醒别人', choices=NOTIFY_CHOICES, coerce=int)
