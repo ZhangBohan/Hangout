182daf552733a3c1449099097d7158ab4d6f94eb
bohanzhang <bohanzhang@foxmail.com>
2016-11-24 07:49:02 +0800

    opt schedule admin

--- GuppiesWechat/hangout/admin.py
+++ GuppiesWechat/hangout/admin.py
@@ -2,8 +2,19 @@ from django.contrib import admin
 
 from hangout.models import *
 
+
+class ScheduleAdmin(admin.ModelAdmin):
+    fields = ['title', 'content', 'user', 'is_notified', 'started_date', 'ended_date']
+    list_display = ('id',
+                    'title',
+                    'user',
+                    'is_notified',
+                    'started_date',
+                    'notify_other',
+                    'accepted_count',)
+
 admin.site.register(Template)
-admin.site.register(Schedule)
+admin.site.register(Schedule, ScheduleAdmin)
 admin.site.register(ScheduleUser)
 admin.site.register(ScheduleShare)
 admin.site.register(HangoutConfig)
