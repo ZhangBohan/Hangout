25190415cb5b090ad0e08d26d293421edf36866b
bohanzhang <bohanzhang@foxmail.com>
2016-11-24 07:29:38 +0800

    add title for page and add admin

--- GuppiesWechat/hangout/admin.py
+++ GuppiesWechat/hangout/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from hangout.models import *
+
+admin.register(Template)
+admin.register(Schedule)
+admin.register(ScheduleUser)
+admin.register(ScheduleShare)
+admin.register(HangoutConfig)
