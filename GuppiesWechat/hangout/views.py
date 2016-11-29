7c08a609e5f9c22222df80e3900848e83d62025e
eyougo <meihongyuan@163.com>
2016-11-29 18:33:25 +0800

    页面滚动bugfix。已约和约我的逻辑修改。增加我的统计数据 #24

--- GuppiesWechat/hangout/views.py
+++ GuppiesWechat/hangout/views.py
@@ -7,11 +7,12 @@ from django.utils import timezone
 
 from hangout.forms import ScheduleForm
 from hangout.models import Schedule, Template, ScheduleShare, ScheduleUser
+from wechat.models import UserInfo
 
 
 @login_required
 def index(request):
-    templates = Template.objects.filter(user=request.user).order_by('-used_count').all()[:10]
+    templates = Template.objects.filter(user=request.user).order_by('-used_count').all()[:15]
     return render(request, 'hangout/index.html', context={
         "templates": templates
     })
@@ -19,32 +20,34 @@ def index(request):
 
 @login_required
 def me(request):
-    query = Schedule.objects.filter(user=request.user)
-    recent_count = query.filter(is_notified__in=[False, None]).count()
+    query = ScheduleUser.objects.filter(user=request.user)
+    recent_query = query.filter(is_notified__in=[False, None]).order_by('-updated_at')
+    joined_recent_count = recent_query.exclude(schedule__user=request.user).count()
+    created_recent_count = recent_query.filter(schedule__user=request.user).count()
     notified_count = query.filter(is_notified=True).count()
-    my_count = ScheduleUser.objects.filter(user=request.user).count()
+    request.user.userinfo = UserInfo()
+    request.user.userinfo.nickname = "mei"
+    request.user.userinfo.avatar_url = "http://bohan.qiniudn.com/2016-07-20_github.png"
     return render(request, 'hangout/me.html', context={
         "user": request.user,
-        "recent_count": recent_count,
-        "notified_count": notified_count,
-        "my_count": my_count
+        "joined_recent_count": joined_recent_count,
+        "created_recent_count": created_recent_count,
+        "notified_count": notified_count
     })
 
 
 @login_required
 def hangout(request):
     # TODO is_notified字段已经迁移到ScheduleUser中，按照ScheduleUser进行查找和统计
-    query = Schedule.objects.filter(user=request.user)
-
-    schedule_users = ScheduleUser.objects.filter(user=request.user).all()
-    my_schedules = [schedule_user.schedule for schedule_user in schedule_users]
-
-    recent_schedules = query.filter(is_notified__in=[False, None]).order_by('-updated_at').all()[:10]
-    notified_schedules = query.filter(is_notified=True).order_by('-updated_at').all()[:10]
+    query = ScheduleUser.objects.filter(user=request.user)
+    recent_query = query.filter(is_notified__in=[False, None]).order_by('-updated_at')
+    joined_recent_schedules = [schedule_user.schedule for schedule_user in recent_query.exclude(schedule__user=request.user).all()[:10]]
+    created_recent_schedules = [schedule_user.schedule for schedule_user in recent_query.filter(schedule__user=request.user).all()[:10]]
+    notified_schedules = [schedule_user.schedule for schedule_user in query.filter(is_notified=True).order_by('-updated_at').all()[:10]]
     return render(request, 'hangout/hangout.html', context={
+        "joined_recent_schedules": joined_recent_schedules,
+        "created_recent_schedules": created_recent_schedules,
         "notified_schedules": notified_schedules,
-        "recent_schedules": recent_schedules,
-        "my_schedules": my_schedules,
     })
 
 
